d4d46ffbb4c8df5d3f98a81c641b13425d1933ed
Jim Carroll <jim@dontcallme.com>
2019-10-05 08:37:27 -0400

    example2

--- xbmc/interfaces/legacy/ModuleExample.h
+++ xbmc/interfaces/legacy/ModuleExample.h
@@ -6,6 +6,7 @@
  */
 
 #include "utils/log.h"
+#include "interfaces/legacy/AddonClass.h"
 
 namespace Something {
 
@@ -13,6 +14,15 @@ namespace Something {
      CLog::Log(LOGNOTICE, "Hello, I'm in a new module.");
    }
 
+  class Example : public XBMCAddon::AddonClass {
+    const char* m_message;
+  public:
+    Example(const char* p_message) : m_message(p_message) {}
+
+    inline void logMe() {
+      CLog::Log(LOGNOTICE, m_message);
+    }
+  };
 }
 
 
